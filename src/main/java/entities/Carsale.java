7f4d834be82e8797d7bf46579ef496f22ac7010f
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-09-15 10:20:10 +0200

    start på carsaleFacade

--- src/main/java/entities/Carsale.java
+++ src/main/java/entities/Carsale.java
@@ -20,35 +20,61 @@ public class Carsale implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private int year;
+    private String make;
+    private String model;
+    private int price;
 
+    public Carsale() {
+    }
+
+    public Carsale(int year, String make, String model, int price) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (int) id;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Carsale)) {
-            return false;
-        }
-        Carsale other = (Carsale) object;
-        if (this.id != other.id) {
-            return false;
-        }
-        return true;
     }
 
     @Override
---
+++ src/main/java/facades/CarsaleFacade.java
@@ -0,0 +1,11 @@
+package facades;
+
+/**
+ *
+ * @author Mathias
+ */
+public class CarsaleFacade {
+    
+    
+    
+}
