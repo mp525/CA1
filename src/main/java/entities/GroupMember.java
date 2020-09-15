6657b33ae79ab7a65bae0734630ef22695374693
MattiBSH <mattibenhansen@gmail.com>
2020-09-15 10:11:35 +0200

    Lavet gruppemedlem entity

---
+++ src/main/java/entities/GroupMember.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author matti
+ */
+@Entity
+public class GroupMember implements Serializable {
+
+    private static final int serialVersionUID = 1;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+    
+}
