2599ba06ab913c00bf2c308d88313a1afd88f602
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-09-15 10:53:56 +0200

    facade test start

---
+++ src/test/java/facades/CarsaleFacadeTest.java
@@ -0,0 +1,50 @@
+package facades;
+
+import dtos.CarsaleDTO;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Mathias
+ */
+public class CarsaleFacadeTest {
+    
+    //private Carsale c1;
+    //private Carsale c1;
+    //private Carsale c1;
+    
+    public CarsaleFacadeTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+    /**
+     * Test of getAllCarsales method, of class CarsaleFacade.
+     */
+    //@Test
+    public void testGetAllCarsales() {
+       
+    }
+    
+}
